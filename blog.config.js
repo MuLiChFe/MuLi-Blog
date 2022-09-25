4a149ce4c520d87eee35750451f8649194ce3915
TGCS <76549977+MuLiChFe@users.noreply.github.com>
2022-09-26 00:39:20 +1000

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
                                                                         // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
-  AUTHOR: 'TGCS', // 作者
-  BIO: 'TGCSurvival.minehut.gg', // 作者简介
-  LINK: 'https://tgc.vercel.app/', // 网站地址
+  AUTHOR: '沐林', // 作者
+  BIO: '沐林不是0', // 作者简介
+  LINK: 'https://mulin.vercel.app/', // 网站地址
   KEYWORDS: '', // 网站关键词 英文逗号隔开
-  NOTION_PAGE_ID: '09710b8e449d41979875baa6818977f4', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
+  NOTION_PAGE_ID: '5239337dbef949e0bb9e574d09a03b7d', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
   DEBUG: process.env.NEXT_PUBLIC_DEBUG || false, // 是否显示调试按钮
 
